e634f569cdf6f9e181921a5e76a4c63d5f24e0e4
asmshahariar <shahariar.arian@gmail.com>
2025-01-27 01:58:16 +0600

    done admin middleware work

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,14 @@ Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('adm
 
 Route::get('/logout', [AdminController::class, 'AdminLogoutpage'])->name('admin.logout.page');
 
-Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 
-Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+
+
+    Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password');
+
+    Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
+    Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
+});
